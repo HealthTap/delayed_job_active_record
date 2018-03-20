6665f946f463af0f9acc8f232c3896ef06545d8c
Ashish Goyal <ashish.goyal@healthtap.com>
2018-03-20 17:26:15 +0530

    archived jobs

--- lib/delayed_job_active_record.rb
+++ lib/delayed_job_active_record.rb
@@ -1,5 +1,7 @@
 require 'active_record'
 require 'delayed_job'
 require 'delayed/backend/active_record'
+require 'delayed/backend/active_record/archived_job'
+require 'delayed/backend/active_record/ht_delayed_job'
 
 Delayed::Worker.backend = :active_record
