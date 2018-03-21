2599de3ada77bbf8624607dd5de1cc9a247f8014
Ashish Goyal <ashish.goyal@healthtap.com>
2018-03-21 09:18:23 +0530

    archive jobs

--- lib/delayed/backend/active_record/ht_delayed_job.rb
+++ lib/delayed/backend/active_record/ht_delayed_job.rb
@@ -2,6 +2,10 @@ module Delayed
   module Backend
     module ActiveRecord
       class HtDelayedJob < ::ActiveRecord::Base
+        include Delayed::Backend::HtExtension
+
+        attr_accessible :priority, :run_at, :queue, :payload_object,
+                        :failed_at, :locked_at, :locked_by
       end
     end
   end
