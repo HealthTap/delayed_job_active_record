0757e9de5af9eae7bf69de49fa95b02b7052d67a
Ashish Goyal <ashish.goyal@healthtap.com>
2018-03-23 13:14:55 +0530

    minor fix

--- lib/delayed_job_active_record.rb
+++ lib/delayed_job_active_record.rb
@@ -2,6 +2,7 @@ require 'active_record'
 require 'delayed_job'
 require 'delayed/backend/active_record'
 require 'delayed/backend/active_record/archived_job'
+require 'delayed/backend/active_record/old_job'
 require 'delayed/backend/active_record/ht_delayed_job'
 
 Delayed::Worker.backend = :active_record
