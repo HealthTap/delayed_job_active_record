bee7b3db29307daafab3439373c7634771faa9ac
DmitryV <dmitry@healthtap.com>
2020-06-22 21:18:32 -0600

    rails upgrade

--- lib/delayed/backend/active_record/archived_job.rb
+++ lib/delayed/backend/active_record/archived_job.rb
@@ -3,9 +3,13 @@ class Delayed::Backend::ActiveRecord::ArchivedJob < ::ActiveRecord::Base
   #                :delayed_object_type, :delayed_object_id, :method_name, :args, :unique_digest,
   #                :attempts, :handler, :last_error
 
+  ATTRIBUTES_TO_MIGRATE = %w(priority run_at queue payload_object failed_at locked_at locked_by
+                 delayed_object_type delayed_object_id method_name args unique_digest
+                 attempts handler last_error)
+
   def self.archive(delayed_job)
     failed_job = Delayed::Backend::ActiveRecord::ArchivedJob.new(
-        delayed_job.attributes.select{|a| Delayed::Backend::ActiveRecord::ArchivedJob.accessible_attributes.include?(a)}
+        delayed_job.attributes.select{|a| ATTRIBUTES_TO_MIGRATE.include?(a)}
     )
     failed_job.failed_at = delayed_job.class.db_time_now
     failed_job.save
@@ -13,7 +17,7 @@ class Delayed::Backend::ActiveRecord::ArchivedJob < ::ActiveRecord::Base
 
   def retry
     retry_job = Delayed::Backend::ActiveRecord::HtDelayedJob.new(
-      self.attributes.select{|a| Delayed::Backend::ActiveRecord::HtDelayedJob.accessible_attributes.include?(a)}
+      self.attributes.select{|a| ATTRIBUTES_TO_MIGRATE.include?(a)}
     )
     retry_job.handler = self.handler
     retry_job.failed_at = nil
